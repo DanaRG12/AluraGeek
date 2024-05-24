e6fca713e1bee242429f006cd2aca9c060935e27
Diana Robles <dana_rg@hotmail.com>
2024-05-23 20:41:29 -0600

    deployed

--- js/services/products-service.js
+++ js/services/products-service.js
@@ -1,11 +1,11 @@
 const productList = () => {
-    return fetch("http://localhost:3000/products")
+    return fetch("https://664f8f9dec9b4a4a602f3692.mockapi.io/api/v1/products")
         .then((res) => res.json())
         .catch((err)=> console.log(err));     
 };
 
 const createProducts = (name, price, image) => {
-    return fetch("http://localhost:3000/products", {
+    return fetch("https://664f8f9dec9b4a4a602f3692.mockapi.io/api/v1/products", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -21,7 +21,7 @@ const createProducts = (name, price, image) => {
 }
 
 const deleteProducts = (id) => {
-    return fetch(`${"http://localhost:3000/products"}/${id}`, {
+    return fetch(`${"https://664f8f9dec9b4a4a602f3692.mockapi.io/api/v1/products"}/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
